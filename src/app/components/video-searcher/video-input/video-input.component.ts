c76b3c17474a71f6639a638fa42f32b384867cbe
ZibiH <zbigniew.holak@gmail.com>
2021-07-06 10:58:00 +0200

    Initial commit

---
+++ src/app/components/video-searcher/video-input/video-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-video-input',
+  templateUrl: './video-input.component.html',
+  styleUrls: ['./video-input.component.scss']
+})
+export class VideoInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
