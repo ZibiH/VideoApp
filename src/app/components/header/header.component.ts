e62852d67a77399cc16a03371210abe423d29d45
ZibiH <zbigniew.holak@gmail.com>
2021-07-06 11:51:20 +0200

    Add header links methods
    Add indicator (underline) to the active route

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+// import { }
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class HeaderComponent {
+  onSelect(event: Event): void {
+    const target = event.target as Element;
+    this.addIndicator(target);
   }
 
+  onLogoClick(): void {
+    const target = document.querySelector(
+      '.header__action-link--accent'
+    ) as Element;
+    this.addIndicator(target);
+  }
+
+  addIndicator(target: Element): void {
+    document.querySelectorAll('.header__action-link').forEach((element) => {
+      if (element.classList.contains('isSelected')) {
+        element.classList.remove('isSelected');
+      }
+    });
+
+    target.classList.add('isSelected');
+  }
 }
