7a750acc6a629425cb0c5422ed1f130becc51824
ZibiH <zbigniew.holak@gmail.com>
2021-07-06 11:48:27 +0200

    Add routes to header links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VideoFilterComponent } from './components/video-playlist/video-filter/video-filter.component';
+import { VideoInputComponent } from './components/video-searcher/video-input/video-input.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'add-video', component: VideoInputComponent, pathMatch: 'full' },
+  { path: 'playlist', component: VideoFilterComponent, pathMatch: 'full' },
+  { path: '', redirectTo: 'add-video', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
