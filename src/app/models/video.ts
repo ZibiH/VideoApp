9e515e77fc95d9019a0fdef8204c23b91e1bf468
ZibiH <zbigniew.holak@gmail.com>
2021-07-09 17:59:48 +0200

    Add Video model

---
+++ src/app/models/video.ts
@@ -0,0 +1,11 @@
+export interface Video {
+  id: string;
+  title: string;
+  description: string;
+  service: string;
+  src: string;
+  likes: string;
+  views: string;
+  favourites: boolean;
+  date: number;
+}
