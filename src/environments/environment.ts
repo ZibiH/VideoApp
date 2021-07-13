ed60101300a0d05dc3f0b4828ee14317ebb198d1
ZibiH <zbigniew.holak@gmail.com>
2021-07-13 16:55:11 +0200

    Add service data to environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,30 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  servicesData: [
+    {
+      name: 'youtube',
+      regEx: '(.*?)(^|/|v=)([a-z0-9_-]{11})(.*)?',
+      flags: 'gim',
+      replaceValue: '$3',
+      iframeUrl: '//www.youtube.com/embed/',
+      apiKey: 'AIzaSyD6aVC6lTRy5FdaGzWIH5Ec2Ol_7oKJLhU',
+      apiParts: 'snippet,statistics',
+      apiUrl: 'https://www.googleapis.com/youtube/v3/videos?id=',
+    },
+    {
+      name: 'vimeo',
+      regEx:
+        '(?:http:|https:|)\\/\\/(?:player.|www.)?vimeo\\.com\\/(?:video\\/|embed\\/|watch\\?\\S*v=|v\\/)?(\\d*)(.*)?',
+      flags: 'g',
+      replaceValue: '$1',
+      iframeUrl: '//player.vimeo.com/video/',
+      apiKey: '',
+      apiParts: '',
+      apiUrl: '',
+    },
+  ],
 };
 
 /*
