573e39e619e7b8363d071d3dafd8373a74ea8a85
ZibiH <zbigniew.holak@gmail.com>
2021-07-13 20:36:56 +0200

    Add fetching video options
    Put credentials in env, add extracting id fn and options in env

--- src/app/services/video-search.service.ts
+++ src/app/services/video-search.service.ts
@@ -2,13 +2,26 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import { environment } from '@environments/environment';
+
 import { Video } from '@app/models/video';
+import { InputData } from '@app/models/input-data';
 
 @Injectable({
   providedIn: 'root',
 })
 export class VideoSearchService {
+  private env = environment;
+  private servData = this.env.servicesData;
   private videoLocalApiUrl = 'http://localhost:5000/videos';
+  private credentials = btoa(
+    `${this.env.credentials.clientId}:${this.env.credentials.clientSecret}`
+  );
+  private vimeoHeaders = {
+    headers: new HttpHeaders({
+      Authorization: `basic ${this.credentials}`,
+    }),
+  };
   videoList: Video[] = [];
 
   constructor(private http: HttpClient) {}
@@ -17,10 +30,38 @@ export class VideoSearchService {
     return this.http.get<Video[]>(this.videoLocalApiUrl);
   }
 
-  fetchVideoApiData(videoInput: {
-    videoUrl: string;
-    videoService: string;
-  }): Observable<Video[]> {
-    return this.http.get<Video[]>(videoInput.videoUrl);
+  fetchVideoApiData(videoData: InputData): Observable<Video[]> {
+    const apiUrl = this.getProperVideoUrl(videoData);
+    if (videoData.videoService === 'vimeo') {
+      return this.http.get<Video[]>(apiUrl, this.vimeoHeaders);
+    }
+    return this.http.get<Video[]>(apiUrl);
+  }
+
+  private getProperVideoUrl(videoData: InputData): string {
+    const videoId: string = this.extractIdFromInputData(videoData);
+    let videoProperUrl: string;
+    switch (videoData.videoService) {
+      case 'youtube':
+        videoProperUrl = `${this.servData[0].apiUrl}${videoId}&key=${this.servData[0].apiKey}&part=${this.servData[0].apiParts}`;
+        break;
+      case 'vimeo':
+        videoProperUrl = `${this.servData[1].apiUrl}${videoId}`;
+        break;
+      default:
+        videoProperUrl = '';
+        break;
+    }
+
+    return videoProperUrl;
+  }
+
+  private extractIdFromInputData(videoData: InputData): string {
+    const { regEx, replaceValue, flags } = this.servData.filter(
+      (data) => data.service === videoData.videoService
+    )[0];
+    const pattern = new RegExp(regEx, flags);
+    const extractedId = videoData.videoUrl.replace(pattern, replaceValue);
+    return extractedId;
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,14 @@
 
 export const environment = {
   production: false,
+  credentials: {
+    clientId: 'ce4c2f556acbe99ecae7bb45deaf14c56e12e650',
+    clientSecret:
+      'Q1qgqlAFtTUAghKVeBT6e9f1MVjjKet5AuONBOccrq10hQGyYbQTi6XgPwVmPE3DK3D14gv8bYgnxvTtmIufb3c/4SW14EhHgte1Oq8dIwfjn90SCraseA5zVLq1wcyp',
+  },
   servicesData: [
     {
-      name: 'youtube',
+      service: 'youtube',
       regEx: '(.*?)(^|/|v=)([a-z0-9_-]{11})(.*)?',
       flags: 'gim',
       replaceValue: '$3',
@@ -16,7 +21,7 @@ export const environment = {
       apiUrl: 'https://www.googleapis.com/youtube/v3/videos?id=',
     },
     {
-      name: 'vimeo',
+      service: 'vimeo',
       regEx:
         '(?:http:|https:|)\\/\\/(?:player.|www.)?vimeo\\.com\\/(?:video\\/|embed\\/|watch\\?\\S*v=|v\\/)?(\\d*)(.*)?',
       flags: 'g',
@@ -24,7 +29,7 @@ export const environment = {
       iframeUrl: '//player.vimeo.com/video/',
       apiKey: '',
       apiParts: '',
-      apiUrl: '',
+      apiUrl: 'https://api.vimeo.com/videos/',
     },
   ],
 };
