f322880ebbcbd80524e9bf19940e1f755db09d02
ZibiH <zbigniew.holak@gmail.com>
2021-07-14 15:08:58 +0200

    Update modules to handle lack of Vimeo views count

--- src/app/models/video.ts
+++ src/app/models/video.ts
@@ -1,11 +1,13 @@
+import { SafeResourceUrl } from '@angular/platform-browser';
+
 export interface Video {
   id: string;
   title: string;
   description: string;
   service: string;
-  src: string;
+  src: SafeResourceUrl;
   likes: string;
-  views: string;
+  views?: string;
   favourites: boolean;
   date: number;
 }
