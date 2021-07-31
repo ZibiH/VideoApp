353c5674f76f38ad163fbb4066c7bfca1d39c185
ZibiH <zbigniew.holak@gmail.com>
2021-07-31 19:52:00 +0200

    Use shared module with shared files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,34 +4,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { MaterialModule } from '@app/material.module';
+import { SharedModule } from './components/shared/shared.module';
 
 import { AppComponent } from '@app/app.component';
-import { DeleteAlertComponent } from '@shared/delete-alert/delete-alert.component';
 import { HeaderComponent } from '@header/header.component';
 import { VideoInputComponent } from '@video-input/video-input.component';
-import { VideoItemComponent } from '@shared/video-item/video-item.component';
 import { VideoListComponent } from '@video-playlist/video-list.component';
-import { ModalComponent } from '@shared/modal/modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DeleteAlertComponent,
     HeaderComponent,
-    ModalComponent,
     VideoInputComponent,
     VideoListComponent,
-    VideoItemComponent,
   ],
-  entryComponents: [DeleteAlertComponent],
   imports: [
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
-    MaterialModule,
     HttpClientModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -16,7 +15,6 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 @NgModule({
   imports: [
     MatButtonModule,
-    MatCardModule,
     MatDialogModule,
     MatDividerModule,
     MatFormFieldModule,
@@ -30,7 +28,6 @@ import { MatTooltipModule } from '@angular/material/tooltip';
   ],
   exports: [
     MatButtonModule,
-    MatCardModule,
     MatDialogModule,
     MatDividerModule,
     MatFormFieldModule,
