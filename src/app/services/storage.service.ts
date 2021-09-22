184c465734471ad3593cc177b31fda329801fd58
ZibiH <zbigniew.holak@gmail.com>
2021-09-22 15:54:14 +0200

    Remove code related to hardcoded video list

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -2,12 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Subject } from 'rxjs';
-// import { map } from 'rxjs/operators';
 
 import { Video } from '@app/models/video';
 import { VideoSearchService } from '@services/video-search.service';
-
-import { videosDemo } from '@assets/videosDemo';
 
 @Injectable({
   providedIn: 'root',
@@ -27,12 +24,8 @@ export class StorageService {
 
   private videosStorageList: Video[] = [];
   private favoriteVideosList: Video[] = [];
-  private videosDemo = videosDemo;
 
-  constructor(
-    private http: HttpClient,
-    private vsService: VideoSearchService
-  ) {}
+  constructor(private vsService: VideoSearchService) {}
 
   getSavedVideos() {
     if (localStorage.getItem(this.videoLocalStorageKey)) {
@@ -129,50 +122,11 @@ export class StorageService {
     this.videosStorageListChange.next(this.videosStorageList);
   }
 
-  // ************************
-  // ****   Server DB    ****
-  // ************************
-
-  // Left here for the optional future use
-  // private addToServerDb(): void {
-  //   this.http
-  //     .post<Video[]>(
-  //       this.videoLocalApiUrl,
-  //       this.videosStorageList,
-  //       this.videoLocalApiHeaders
-  //     )
-  //     .subscribe((response) => console.log(response));
-  // }
-
   getLocalDbVideos(): void {
-    // ********************************************************
-    // Uncomment this part to use local server data (port:5000)
-    // ********************************************************
-
-    // this.http
-    //   .get<Array<Video[]>>(this.videoLocalApiUrl)
-    //   .pipe(map((videoArray: Video[][]) => videoArray[0]))
-    //   .subscribe(
-    //     (videoList: Video[]) => {
-    //       videoList.forEach((video: Video) => {
-    //         const vidItem = video;
-    //         vidItem.safeSrc = this.vsService.sanitizeVideoSrc(vidItem.src);
-    //         this.addVideoToList(vidItem);
-    //       });
-    //       this.getSavedVideos();
-    //       this.getFavoritesVideos();
-    //     },
-    //     (error) => console.log(error.message)
-    //   );
-
-    // *****************************************************
-    // Comment-out this part if you want to use local server
-    // *****************************************************
-
-    this.videosDemo.forEach((video: Video) => {
-      const vidItem = video;
-      vidItem.safeSrc = this.vsService.sanitizeVideoSrc(vidItem.src);
-      this.addVideoToList(vidItem);
+    this.vsService.fetchDefaultVideoBase().subscribe((vid) => {
+      this.videosStorageList = vid;
+      this.videosStorageListChange.next(this.videosStorageList);
+      this.setLocalStorageVideoItem();
     });
     this.getSavedVideos();
     this.getFavoritesVideos();
